02b48d5aa7cfa2dd99bcf997ea787341ff30b4b5
Clayton Mercer <mercerccw@gmail.com>
2020-03-19 19:28:01 -0400

    Added function to send message (not finished)

--- PortAuthority/src/main/java/beans/NoticeManager.java
+++ PortAuthority/src/main/java/beans/NoticeManager.java
@@ -4,13 +4,24 @@ import com.google.gson.Gson;
 
 import java.io.Serializable;
 
+import javax.annotation.Resource;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
+import javax.jms.*;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 @SessionScoped
 @Named("noticeMgr")
 public class NoticeManager implements Serializable {
 
+	@PersistenceContext(name="myShips")
+	EntityManager em;
+
+	@Resource(mappedName="jms/myLog")
+	private Queue logMessages;
+	@Resource(mappedName="jms/myMessageFactory")
+	private ConnectionFactory logFactory;
 	
 	private static final long serialVersionUID = 1L;
 
@@ -23,10 +34,22 @@ public class NoticeManager implements Serializable {
 		Gson gson = new Gson();
 		return gson.toJson(notice);
 	}
-	
+	private void sendMessage(String msg){
+		try (JMSContext context = logFactory.createContext()){
+			JMSProducer mp = context.createProducer();
+			Message tm = context.createTextMessage(msg);
+			mp.send(logMessages,tm);
+		} catch( Exception e) {
+			System.out.println("Error: could not send message "+ e);
+		}
+	}
 	public NoticeOfArrival getNotice() { return notice ; }
 
 	
-	public void mesg() { System.out.println(serializeJSON(notice));}
+	public void mesg() {
+		String msg = serializeJSON(notice);
+		sendMessage(msg);
+		System.out.println(msg);
+	}
 
 }
