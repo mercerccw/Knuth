25c2a0ee887bf3753834e3d57571d5a8eb00b28b
Eli <elisokeland@yahoo.com>
2020-04-26 00:06:05 -0500

    Attempt to get Recent AIS Messages on Frontend

--- trafficmonitor/src/App.js
+++ trafficmonitor/src/App.js
@@ -1,4 +1,5 @@
 import React, {useEffect, useState} from 'react';
+import moment from 'moment';
 import Request from './Requests';
 import './App.css';
 import {Map, Marker, Popup, TileLayer} from "react-leaflet";
@@ -17,10 +18,22 @@ const anchor = new Icon({
 });
 
 function App() {
-    const [aisMessages, updateAISMessages] = useState(null);
+    const [aisMessages, updateAISMessages] = useState([]);
+     const [timestamp, updateTimestamp] = useState(new Date("2018-09-11T10:38:00Z"));
 
     useEffect(() => {
-        Request.getAllAISMessages().then(res => updateAISMessages(res.data));
+    }, []);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            let momentTime = moment(timestamp);
+            let formattedTimestamp = momentTime.toISOString();
+            Request.getRecentAISMessages(formattedTimestamp).then(res => {
+                updateAISMessages(res.data)
+            }).catch(error => console.log(error.message));
+            updateTimestamp(momentTime.add(1, 's'));
+        }, 1000);
+        return () => clearInterval(interval);
     }, []);
 
     return (
@@ -30,6 +43,11 @@ function App() {
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         />
+          {aisMessages.length > 0 && aisMessages.map((aisMessage) => {
+              return <Marker position={[aisMessage.PositionReport.Position.coordinates[1], aisMessage.PositionReport.Position.coordinates[0]]} icon={anchor}>
+                  <Popup>VESSEL</Popup>
+              </Marker>
+          })}
           <Marker position={[55.626233, 13.016933]} icon={anchor}>
               <Popup>Vessel information here.</Popup>
           </Marker>
